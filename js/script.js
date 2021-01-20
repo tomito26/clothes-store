010d06ecfa954fc3a1278a17f246f3f478f08c01
tomito26 <tommybwah@gmail.com>
2021-01-20 07:38:30 +0300

    added javascript that displays the total price of the items in the cart and also styled the cart.html

--- js/script.js
+++ js/script.js
@@ -128,20 +128,40 @@ function totalCost(product){
 function displayCart(){
     let cartItems = localStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems)
-    let productContainer = document.querySelector(".products-container")
-    
+    let productContainer = document.querySelector(".products")
+    let cartCost = localStorage.getItem("totalCost");
 
     if(cartItems && productContainer){
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item=>{
             productContainer.innerHTML += `
-            <div class="product>
-                <ion-icon name="close-circle-sharp"></ion-icon>
+            <div class="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
                 <img src="assets/${item.tag}.jpeg">
                 <span>${item.name}</span> 
             </div>
+            <div class="price">$${item.price}.00</div>
+            <div class="quantity">
+                <ion-icon name="chevron-back-circle-outline"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon name="chevron-forward-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price}.00
+            </div>
             `;
-        })
+        });
+        productContainer.innerHTML +=`
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle">
+                Basket Total            
+            </h4>
+            <h4 class="basketTotal">
+                $${cartCost}.00
+            </h4>
+        
+        </div>
+        `
     }
 };
 
